ab270b2aa3565dc8b19b744c4e934e31bd59a119
developer <dev@renocms.ru>
2026-05-02 14:13:36 +0500

    flush cache event

--- src/FiltersAdapterServiceProvider.php
+++ src/FiltersAdapterServiceProvider.php
@@ -5,14 +5,13 @@ namespace Reno\FiltersAdapter;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Filters\Services\FiltersPrecacheService;
+use Reno\Cms\Events\Resources\CmsCacheFlushed;
 
 class FiltersAdapterServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        Event::listen([
-            // event cmscache clear
-        ], function () {
+        Event::listen(CmsCacheFlushed::class, function () {
             /** @var FiltersPrecacheService $service */
             $service = resolve(FiltersPrecacheService::class);
             $service->dropCache();
